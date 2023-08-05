e577ceab38e041732958776248dcc15549fe189e
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-08-05 23:15:16 +0530

    Add files via upload
    queues

---
+++ shivam.java
@@ -0,0 +1,13 @@
+public class shivam {
+    public static void main(Strings[] args){
+        int x = 5;
+        int m = 5;
+        for(int i = 1; i<=x; i++){
+            for(int j = 1; j<=m; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
